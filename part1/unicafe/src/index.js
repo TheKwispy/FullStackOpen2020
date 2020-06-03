5965f0ea02c1fc671aadbb076c03bfe1a2f84cd9
TheKwispy <kristian.martin94@protonmail.com>
2020-06-03 20:27:40 +0300

    Completed 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementGood = () => {
+    setGood(good + 1);
+  };
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const incrementBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => incrementGood()}>Good</button>
+      <button onClick={() => incrementNeutral()}>Neutral</button>
+      <button onClick={() => incrementBad()}>Bad</button>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
